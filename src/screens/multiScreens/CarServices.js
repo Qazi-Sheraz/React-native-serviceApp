a753f4d8340e99b4e7d7cf5b7a1ad65bdb8eebc8
Qazi-Sheraz <qazisheraz2@gmail.com>
2022-10-14 21:47:21 +0500

    car

---
+++ src/screens/multiScreens/CarServices.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {View} from 'react-native';
+import {NavHeader} from '../../components';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
+
+
+
+
+
+export class CarServices extends React.Component {
+    render() {
+      return (
+        <View
+        style={{
+            backgroundColor:'red',
+            flex:1
+        }}></View>
+     
+    );
+   }
+}
